e8f9c6b9e5a87a4c738852ba4032ec84ed093701
Morlay <morlay.null@gmail.com>
2020-09-09 10:56:24 +0800

    chore: ci fix

--- pkg/version/git_commit_test.go
+++ pkg/version/git_commit_test.go
@@ -7,7 +7,10 @@ import (
 )
 
 func TestListCommit(t *testing.T) {
-	lastVersion, _, _ := LastVersion()
+	lastVersion, _, err := LastVersion()
+	if err != nil {
+		return
+	}
 	spew.Dump(lastVersion)
 	spew.Dump(ListCommit("v" + lastVersion.String()))
 }
